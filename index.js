546fdd2549e125be8163ca0987653ecb3a519f5e
Sanjoy9800 <sanjurudra4@gmail.com>
2023-06-22 20:37:51 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const hr=document.getElementById('hour');
+const mi=document.getElementById('min');   
+const se=document.getElementById('sec');
+const ticSound=document.getElementById('tictic')
+// const sound=new Audio('sound.mp3');
+
+
+const disTime=()=>{
+  let date=new Date();
+  let hh=date.getHours();
+  let mm=date.getMinutes();
+  let ss=date.getSeconds();
+
+  let hRotation=30*hh + mm/2;
+  let mRotation=6*mm;
+  let sRotation=6*ss;
+
+  hr.style.transform= `rotate(${hRotation}deg)`;
+  mi.style.transform= `rotate(${mRotation}deg)`;
+  se.style.transform= `rotate(${sRotation}deg)`;
+
+  // sound.currentTime=0;
+  // sound.play()
+
+
+  // let sound =new Audio('sound.mp3')
+  // sound.play()
+
+  ticPlay();
+
+}
+
+const ticPlay=()=>{
+  ticSound.currentTime=0;
+  ticSound.play();
+}
+
+setInterval(disTime,1000)
+
+
